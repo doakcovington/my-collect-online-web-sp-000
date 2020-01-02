472162bf2c180e121775cea9c04f4efbd2121bc7
Doak Covington <doakcovington@protonmail.com>
2020-01-02 02:35:39 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -6,6 +6,7 @@ def my_collect(collection)
 
   while i < collection.length
       new_collection << yield(collection[i])
+      i += 1
   end #end of while loop
   new_collection
 end #end of my collect method
